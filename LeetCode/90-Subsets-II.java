125df26dd23bc46610dbcdda81bc71383934d226
Himanshu Choudhary <himanshu.choudhary_cs23@gla.ac.in>
2026-07-03 00:05:55 +0530

    [90] [Time Beats: 56.69%] [Memory Beats: 88.91%] - LeetPush

---
+++ LeetCode/90-Subsets-II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>(); 
+        Arrays.sort(nums);
+      
+        subset(nums,0,list,ans);
+        return ans;
+
+    }
+    static void subset(int []nums,int idx,List<Integer> list,List<List<Integer>> ans)
+    {
+       
+        
+        ans.add(new ArrayList<>(list));
+        for(int i=idx;i<nums.length;i++)
+        {
+
+            if(i>idx && nums[i]==nums[i-1]) continue;
+
+            list.add(nums[i]);
+            subset(nums,i+1,list,ans);
+            list.remove(list.size()-1);
+        }
+
+    }
+}
